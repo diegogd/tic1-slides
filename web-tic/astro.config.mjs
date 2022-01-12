9c8ba97f6877174e7863ea3e582408f6a74739e6
Diego Gomez Diaz <diego.gomez@escuelaideo.edu.es>
2022-01-12 16:41:58 +0100

    Changed main website structure

--- web-tic/astro.config.mjs
+++ web-tic/astro.config.mjs
@@ -9,8 +9,7 @@ export default {
     sitemap: true,         // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
+    
   },
   renderers: [],
 };
